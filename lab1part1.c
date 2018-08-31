e0b7d6afe6af326649245f564bb8d89541cfeb52
Aaron Musgrave <aaron@winthropdc.com>
2018-08-31 15:11:02 +0800

    Part 2(b) done

--- lab1part1.c
+++ lab1part1.c
@@ -7,7 +7,7 @@
  * Run:     mpiexec ...
  *
  * Input:   Number of "dart tosses"
- * Output:	Estimate of pi.  If DEBUG flag is set, results of processes' tosses.
+ * Output:  Estimate of pi.  If DEBUG flag is set, results of processes' tosses.
  */
 
 #include <stdio.h>
@@ -23,7 +23,7 @@ int main(void) {
     long long int local_number_of_tosses;
     long long int number_in_circle;
     long long int local_number_in_circle;
-    
+
     double pi_estimate;
     int my_rank, comm_sz;
     MPI_Comm comm;
@@ -32,24 +32,24 @@ int main(void) {
     comm = MPI_COMM_WORLD;
     MPI_Comm_size(comm, &comm_sz);
     MPI_Comm_rank(comm, &my_rank);
-    
+
     Get_input(&number_of_tosses, my_rank, comm);
     local_number_of_tosses = number_of_tosses/comm_sz;
 
-#	ifdef DEBUG
+#   ifdef DEBUG
         printf("Proc %d > number of tosses = %lld, local number = %lld\n", my_rank, number_of_tosses, local_number_of_tosses);
-#	endif
+#   endif
 
     local_number_in_circle = Monte_carlo(local_number_of_tosses, my_rank);
-    
+
     /* Compute global sum of local_number_in_circle and store total in variable number_in_circle in process 0*/
     MPI_Reduce(&local_number_in_circle, &number_in_circle, 1, MPI_LONG_LONG_INT, MPI_SUM, 0, MPI_COMM_WORLD);
-    
+
     if ( my_rank == 0 ) {
         pi_estimate = 4*number_in_circle/((double)number_of_tosses);
         printf("pi estimate = %f\n", pi_estimate);
     }
-    
+
     MPI_Finalize();
     return 0;
 }	/* main */
@@ -60,12 +60,12 @@ void Get_input(
         long long int* number_of_tosses /* out */,
         int my_rank                     /* in  */, 
         MPI_Comm comm                   /* in  */) {
-        
+
     if (my_rank == 0 ) {
         printf("Enter the total number of tosses\n");
         scanf("%lld", number_of_tosses);
     }
-    
+
     MPI_Bcast(number_of_tosses, 1, MPI_LONG_LONG_INT, 0, MPI_COMM_WORLD);
 }   /* Get_input */
 
@@ -76,22 +76,22 @@ long long int Monte_carlo(long long local_number_of_tosses, int my_rank) {
     double x,y;
     double distance_squared;
     long long int number_in_circle = 0;
-        
+
     srandom(my_rank+1);
     for ( i=0 ; i< local_number_of_tosses ; i++) {
         x = (random()*2.0 / RAND_MAX) - 1.0;    /* x = random double between -1 and 1 */
         y = (random()*2.0 / RAND_MAX) - 1.0;    /* y = random double between -1 and 1 */
-        
+
         distance_squared = (x*x) + (y*y);       /* square of dart's distance from origin */
-#	ifdef DEBUG
+#   ifdef DEBUG
         printf("Proc %d > x = %f, y = %f, dist squared = %f\n", my_rank, x, y, distance_squared);
-#	endif
+#   endif
         /* if dart falls in unit circle, increment the count number_in_circle */
         if (distance_squared < 1) {
             number_in_circle++;
         }
     }
-    
+
     return number_in_circle;
 }   /* Monte_carlo */
 
--- lab1part2.c
+++ lab1part2.c
@@ -20,36 +20,36 @@ int Global_sum(int my_int, int my_rank, int comm_sz, MPI_Comm comm);
 const int MAX_CONTRIB = 20;
 
 int main(void) {
-   int i, sum, my_int;
-   int my_rank, comm_sz;
-   MPI_Comm comm;
-   int* all_ints = NULL;
-   
-   MPI_Init(NULL, NULL);
-   comm = MPI_COMM_WORLD;
-   MPI_Comm_size(comm, &comm_sz);
-   MPI_Comm_rank(comm, &my_rank);
+    int i, sum, my_int;
+    int my_rank, comm_sz;
+    MPI_Comm comm;
+    int* all_ints = NULL;
+    
+    MPI_Init(NULL, NULL);
+    comm = MPI_COMM_WORLD;
+    MPI_Comm_size(comm, &comm_sz);
+    MPI_Comm_rank(comm, &my_rank);
 
-   srandom(my_rank + 1);
-   my_int = random() % MAX_CONTRIB;
-   
-   sum = Global_sum(my_int, my_rank, comm_sz, comm);
-   
-   if ( my_rank == 0) {
-      all_ints = malloc(comm_sz*sizeof(int));
-      /* Gather from each process each my_int to send back to process 0 to store all summands in array all_ints*/
-      printf("Ints being summed:\n   ");
-      for (i = 0; i < comm_sz; i++)
-         printf("%d ", all_ints[i]);
-      printf("\n");
-      printf("Sum = %d\n",sum);
-      free(all_ints);
-   } else {
-      /* Gather from each process each my_int to send back to process 0 to store all summands in array all_ints*/
-   }
-   
-   MPI_Finalize();
-   return 0;
+    srandom(my_rank + 1);
+    my_int = random() % MAX_CONTRIB;
+    
+    sum = Global_sum(my_int, my_rank, comm_sz, comm);
+    
+    if ( my_rank == 0) {
+        all_ints = malloc(comm_sz*sizeof(int));
+        /* Gather from each process each my_int to send back to process 0 to store all summands in array all_ints*/
+        printf("Ints being summed:\n   ");
+        for (i = 0; i < comm_sz; i++)
+            printf("%d ", all_ints[i]);
+        printf("\n");
+        printf("Sum = %d\n",sum);
+        free(all_ints);
+    } else {
+        /* Gather from each process each my_int to send back to process 0 to store all summands in array all_ints*/
+    }
+    
+    MPI_Finalize();
+    return 0;
 }  /* main */
 
 /*-------------------------------------------------------------------
@@ -59,29 +59,31 @@ int main(void) {
  * 1.  The return value is only valid on process 0
  */
 int Global_sum(
-      int my_int    /* in */,
-      int my_rank   /* in */,
-      int comm_sz   /* in */,
-      MPI_Comm comm /* in */) {
-   
-   int partner, recvtemp;
-   int my_sum = my_int;
-   unsigned bitmask = 1;
-   
-   while (bitmask < comm_sz) {
-      partner = my_rank ^ bitmask;
-      
-      if (my_rank < partner) {
-         if (partner < comm_sz) {
-            /* Recv value from partner into recvtemp */ 
-            my_sum += recvtemp;
-         }
-         bitmask <<= 1;
-      } else {
-         /* Send value in my_sum to partner process */ 
-         break;
-      }
-   }  /* while */
+        int my_int    /* in */,
+        int my_rank   /* in */,
+        int comm_sz   /* in */,
+        MPI_Comm comm /* in */) {
+    
+    int partner, recvtemp;
+    int my_sum = my_int;
+    unsigned bitmask = 1;
+    
+    while (bitmask < comm_sz) {
+        partner = my_rank ^ bitmask;
+        
+        if (my_rank < partner) {
+            if (partner < comm_sz) {
+                /* Recv value from partner into recvtemp */ 
+                MPI_Recv(&recvtemp, 1, MPI_INT, partner, 0, comm, MPI_STATUS_IGNORE);
+                my_sum += recvtemp;
+            }
+            bitmask <<= 1;
+        } else {
+            /* Send value in my_sum to partner process */ 
+            MPI_Send(&my_sum, 1, MPI_INT, partner, 0, comm);
+            break;
+        }
+    }  /* while */
 
-   return my_sum;
+    return my_sum;
 }  /* Global_sum */
